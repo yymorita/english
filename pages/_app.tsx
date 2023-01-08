15bc2a695c3e1f589ad329bfec6a5e6a57e41822
Richard Morita <richard@pulp.conoha.io>
2023-01-08 17:29:08 +0900

    recommit.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/global.css'
+import localFont from '@next/font/local'
 import { AppProps } from 'next/app'
+
+const IBMPlexSansJP = localFont({src: '../public/IBM-Plex-Sans-JP/hinted/IBMPlexSansJP-Text.otf'})
 
 function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
