51920b7936f732cc84669a60427b32579130571f
Richard Morita <richard@pulp.conoha.io>
2023-04-02 22:53:46 +0900

    Update: menu bar.

--- components/layout.jsx
+++ components/layout.jsx
@@ -12,7 +12,11 @@ function Layout({ children }) {
                 <title>英語学習の記録</title>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </Head>
-            <div className={utilStyles.leftSide}><Link href={`/`}>英語学習の記録</Link> | <Link href={'/tags'}>タグ</Link></div>
+            <div className={utilStyles.leftSide}>
+                <Link href={`/`}>英語学習の記録</Link> | 
+                <Link href={'/tags'}>タグ</Link> |
+                <Link href='https://daily.cocno.co/posts/vt3ormwet'>英語のハノン</Link>
+            </div>
             {children}
         </div>
     )
