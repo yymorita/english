18c4c0da63bcf9f0f47127e177e35e3ffbe600ac
Richard Morita <richard@pulp.conoha.io>
2023-04-03 22:07:37 +0900

    Fix: nav bar.

--- components/layout.jsx
+++ components/layout.jsx
@@ -14,8 +14,8 @@ function Layout({ children }) {
             </Head>
             <div className={utilStyles.leftSide}>
                 <Link href={`/`}>英語学習の記録</Link> | 
-                <Link href={'/tags'}>タグ</Link> | 
-                <Link href='https://daily.cocno.co/posts/vt3ormwet'>英語のハノン</Link>
+                <Link href={'/tags'}> タグ</Link> |
+                <Link href='https://daily.cocno.co/posts/vt3ormwet'> 英語のハノン</Link>
             </div>
             {children}
         </div>
